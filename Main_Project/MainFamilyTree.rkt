84801fee937c4fa3f59b87fcee868003c38156bc
Jahid H Emon <je2264r@gre.ac.uk>
2025-02-25 15:34:24 +0000

    Updated the main project

--- Main_Project/MainFamilyTree.rkt
+++ Main_Project/MainFamilyTree.rkt
@@ -13,6 +13,44 @@
 ((John Bloom) ((Greta Blake) (Ned Bloom)) ((5 12 2023) ()))))
 
 
+;,Paternal branch
+(define Pb
+'(((John Smith) ((Jane Doe) (Fred Smith)) ((1 12 1956) (3 3 2021))) 
+((Ana Smith) ((Jane Doe) (Fred Smith)) ((6 10 1958) ()))
+((Jane Doe) ((Eve Talis) (John Doe)) ((2 6 1930) (4 12 1992)))
+((Fred Smith) ((Lisa Brown) (Tom Smith)) ((17 2 1928) (13 9 2016)))
+((Eve Talis) (() ()) ((15 5 1900) (19 7 1978)))
+((John Doe) (() ()) ((18 2 1899)(7 7 1970)))
+((Lisa Brown) (() ())((31 6 1904) (6 3 1980)))
+((Tom Smith) (() ()) ((2 8 1897) (26 11 1987)))
+((Alan Doe) ((Eve Talis) (John Doe)) ((8 9 1932) (23 12 2000)))
+((Mary Doe) (() (Alan Doe)) ((14 4 1964) ()))))
+
+;;define lst-mb
+;;define lst-pb
+;;define lst-all
+
+;; C1
+(define (lst-mb mb)
+  mb)
+ 
+;; C2 
+(define (lst-pb pb)
+  pb)
+  
+;; C3
+(define (append-lst list1 list2)
+        (if (null? list1) list2
+            (cons (car list1) (append-lst (cdr list1) list2))))
+			
+(define (lst-all mb pb)
+  (append-lst mb pb))
+(lst-all Mb Pb)
+
+
+
+
+
 
 
                                            #| Features -> A |#
